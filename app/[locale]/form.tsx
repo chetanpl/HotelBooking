6a1fff5fb949cd7c7485f5445a7064385bae226f
Chetan pal <chetanpl@gmail.com>
2025-06-29 16:06:15 +0100

    update api endpoints

--- app/[locale]/form.tsx
+++ app/[locale]/form.tsx
@@ -187,7 +187,7 @@ const payload = {
     },
   };
   try {
-    const apiUrl:string = process.env.API_URL || 'http://localhost:3000/api/booking';
+    const apiUrl:string = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/booking';
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
