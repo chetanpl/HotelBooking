aacfb22814992811bda3740d85ac6a02e9d73974
Chetan pal <chetanpl@gmail.com>
2025-06-29 18:12:50 +0100

    updated the env file

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,8 +6,9 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-    const apiUrl =
-      process.env.NEXT_PUBLIC_API_URL || 'https://hotel-booking-sigma-ashen.vercel.app/api/booking';
+  const apiUrl =
+    process.env.NEXT_PUBLIC_API_URL || 'https://hotel-booking-sigma-ashen.vercel.app/api/booking';
+
   try {
     const res = await fetch(apiUrl, { cache: 'no-store' });
 
